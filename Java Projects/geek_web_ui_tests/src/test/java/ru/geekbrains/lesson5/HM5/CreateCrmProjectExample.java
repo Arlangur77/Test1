07967b825b81984873b1cb5f936911a4b6efac37
Arlangur77 <alex-07.07.87@mail.ru>
2021-09-06 19:23:17 +0300

    Добавил ДЗ 6

--- Java Projects/geek_web_ui_tests/src/test/java/ru/geekbrains/lesson5/HM5/CreateCrmProjectExample.java
+++ Java Projects/geek_web_ui_tests/src/test/java/ru/geekbrains/lesson5/HM5/CreateCrmProjectExample.java
@@ -32,69 +32,62 @@ public class CreateCrmProjectExample {
         @BeforeEach
         void setupBrowser() {
             driver = new ChromeDriver();
-            WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
-            driver.manage().window().maximize();
+            WebDriverWait webDriverWait = new WebDriverWait(driver, 4);
             loginToCrm();
         }
 
        @Test
        void checkButtonCreateNewProject() throws InterruptedException {
-        driver.get("https://crm.geekbrains.space/project/my");
-        Actions actions = new Actions(driver);
-        WebElement projectMenuElement = driver.findElement(By.xpath("//a/span[text()='Проекты']"));
-        actions.moveToElement(projectMenuElement).perform();
+           driver.get("https://crm.geekbrains.space/project/");
+           Actions actions = new Actions(driver);
+           WebElement projectMenuElement = driver.findElement(By.xpath("//a/span[text()='Проекты']"));
+           actions.moveToElement(projectMenuElement).perform();
 
-        driver.findElement(By.xpath("//li[@data-route='crm_project_index']/a")).click();
+           webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Создать проект']")));
+           driver.findElement(By.xpath("//a[text()='Создать проект']")).click();
+           Thread.sleep(5000);
 
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Создать проект']")));
-        driver.findElement(By.xpath("//a[text()='Создать проект']")).click();
+           webDriverWait.until(ExpectedConditions.elementToBeClickable(By.name("crm_project[name]")));
+           driver.findElement(By.name("crm_project[name]")).sendKeys("crm_project[name]");
 
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.name("crm_project[name]")));
-        driver.findElement(By.name("crm_project[name]")).sendKeys("crm_project[name]");
+           driver.findElement(By.xpath("//span[text()='Укажите организацию']")).click();
+           driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("test");
 
-        driver.findElement(By.xpath("//span[text()='Укажите организацию']")).click();
-        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("test");
+           webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='select2-result-label']")));
+           List<WebElement> organizationVars = driver.findElements(By.xpath("//div[@class='select2-result-label']"));
+           organizationVars.get(0).click();
 
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='select2-result-label']")));
-        List<WebElement> organizationVars = driver.findElements(By.xpath("//div[@class='select2-result-label']"));
-        organizationVars.get(0).click();
+           webDriverWait.until(ExpectedConditions.elementToBeClickable(
+                   By.xpath("//div[contains(@id, 's2id_crm_project_contactMain-uid')]/a")));
+           webDriverWait.until(ExpectedConditions.textToBePresentInElement(
+                   driver.findElement(By.xpath("//div[contains(@id, 's2id_crm_project_company')]/a")), "123test"));
+           Thread.sleep(1000);
+           webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='select2-container select2']")));
+           driver.findElement(By.xpath("//div[contains(@id, 's2id_crm_project_contactMain-uid')]/a")).click();
+           webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='select2-drop']//input")));
+           driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("1111");
+           driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys(Keys.ENTER);
 
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//div[contains(@id, 's2id_crm_project_contactMain-uid')]/a")));
-        webDriverWait.until(ExpectedConditions.textToBePresentInElement(
-                driver.findElement(By.xpath("//div[contains(@id, 's2id_crm_project_company')]/a")), "123test"));
-        Thread.sleep(1000);
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='select2-container select2']")));
-        driver.findElement(By.xpath("//div[contains(@id, 's2id_crm_project_contactMain-uid')]/a")).click();
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='select2-drop']//input")));
-        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("1111");
-        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys(Keys.ENTER);
+           Select businessUnitSelect = new Select(driver.findElement(By.name("crm_project[businessUnit]")));
+           businessUnitSelect.selectByVisibleText("Research & Development");
 
-        Select businessUnitSelect = new Select(driver.findElement(By.name("crm_project[businessUnit]")));
-        businessUnitSelect.selectByVisibleText("Research & Development");
+           Select Curator = new Select(driver.findElement(By.name("crm_project[curator]")));
+           Curator.selectByVisibleText("Applanatest2 Applanatest2 Applanatest2");
 
-        Select Curator = new Select(driver.findElement(By.name("crm_project[curator]")));
-        Curator.selectByVisibleText("Applanatest2 Applanatest2 Applanatest2");
+           Select Head = new Select(driver.findElement(By.name("crm_project[rp]")));
+           Head.selectByVisibleText("Гатов Фёдор");
 
-        Select Head = new Select(driver.findElement(By.name("crm_project[rp]")));
-        Head.selectByVisibleText("Гатов Фёдор");
+           Select Manager = new Select(driver.findElement(By.name("crm_project[manager]")));
+           Manager.selectByVisibleText("Воденеева Алла");
 
-        Select Manager = new Select(driver.findElement(By.name("crm_project[manager]")));
-        Manager.selectByVisibleText("Воденеева Алла");
+           Thread.sleep(5000);
 
-        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@id, 'crm_project_planning-uid')]")));
-        driver.findElement(By.xpath("//body")).sendKeys("testtest");
 
-        Thread.sleep(5000);
-    }
+           driver.get("https://crm.geekbrains.space/project/create/");
+           driver.findElement(By.xpath("//button[contains(text(),'Сохранить и закрыть')]")).click();
+               Thread.sleep(5000);
 
-    @Test
-    void checkButtonSafeAndClose () throws InterruptedException {
-        driver.get("https://crm.geekbrains.space/project/create/");
-        Actions actions = new Actions(driver);
-        driver.findElement(By.xpath("//button[contains(text(),'Сохранить и закрыть')]")).click();
-        Thread.sleep(5000);
-    }
+       }
 
     @AfterEach
     void tearDown() {
